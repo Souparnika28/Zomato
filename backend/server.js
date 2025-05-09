cbf86023812c31b2c043ed92771fe740d3933c9a
Souparnika28 <souparnikasaji123@gmail.com>
2025-05-09 17:26:12 +0100

    added_comments

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,6 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// This code creates a simple Express server with API routes and JSON support.
+// This code is created by me with the help of youtube tutorials.
